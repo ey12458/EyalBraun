5e7ca2fdfb9c0d3b78448b503274dc3ff100f3f9
ey12458 <eybral@gmail.cim>
2026-01-21 12:12:28 +0200

    DB

--- EyalBraun/signup - Copy.aspx.cs
+++ EyalBraun/signup - Copy.aspx.cs
@@ -7,8 +7,21 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
+    string stResult = "";
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            string username = Request.Form["username"];
+            string email = Request.Form["email"];
+            string password = Request.Form["password"];
 
+            string sqlInsert = 
+                "insert into tUsers" + "values (" +  "n'" +username + "', " + "n'" + email + "', " + "n'"+ password + ")";
+            MyAdoHelper.DoQuery("MyDB.mdf", sqlInsert);
+            stResult = "User " + username + " added successfully.";
+
+
+        }
     }
 }
