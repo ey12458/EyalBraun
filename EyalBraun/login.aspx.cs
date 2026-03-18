e28449fcb42650a17248c4930951fbf6870ba017
ey12458 <eybral@gmail.com>
2026-03-18 11:29:32 +0200

    Add admin login, user authentication, and admin page
    Implemented login logic in login.aspx.cs for both admin and regular users, including hardcoded admin credentials and user validation against the database. Created new adminpage.aspx and adminpage.aspx.cs for the admin interface. Fixed CSS font-size property in MasterPage.master. Updated MyDB.mdf to support authentication changes.

--- EyalBraun/login.aspx.cs
+++ EyalBraun/login.aspx.cs
@@ -7,8 +7,39 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
+    public string st = "";
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (Page.IsPostBack)
+        { 
+        
+        string email= Request.Form["email"];
+            string password =Request.Form["password"];
+
+            //התחברות מנהל
+            if (email == "eybral@gmail.com" && password == "12345678")
+            {
+                Response.Redirect("adminpage.aspx");
+            }
+            else { 
+            string sql= "select * from users where email='" + email + "' and password='" + password + "'";
+                bool userExist = MyAdoHelper.IsExist(sql);
+
+                if (!userExist) { 
+                st= "אימייל או סיסמא לא נכונים";
+                }
+                else
+                {
+                    //התחברות משתמש רגיל
+                    Response.Redirect("homepage.aspx");
+                }
+
+            }
+
+
+
+
+        }
 
     }
 }
